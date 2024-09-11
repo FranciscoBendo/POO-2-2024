1eb7a8ef14b617538060e137141540366c274ebc
Francisco N. Bendô <bendo.contato@gmail.com>
2024-09-11 10:31:02 -0300

    Update Test.java

--- src/collections/Test.java
+++ src/collections/Test.java
@@ -6,15 +6,15 @@ public class Test {
     public static void main(String[] args) {
     ArrayList<Usuario> usuarios = new ArrayList<>();
 
-    usuario u1 = new Usuario("Victor", "dev@gmail.com", "dev");
+    Usuario u1 = new Usuario("Victor", "dev@gmail.com", "dev");
     usuarios.add(u1);
     usuarios.add(new Usuario("João", "Joao@gmail.com", "jao"));
-    usuario u2 = new Usuario("Paulo", "Paulo@gmail.com", "Pau");
+    Usuario u2 = new Usuario("Paulo", "Paulo@gmail.com", "Pau");
     usuarios.add(u2);
 
     System.out.println(usuarios.stream().count());
 
-    System.out.println(usuarios.gets(2).nome);
+    System.out.println(usuarios.get(2).nome);
 
     for(Usuario usuario : usuarios){
         System.out.println("Nome: " + usuario.nome);
